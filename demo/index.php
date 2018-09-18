43b92014e8dd67ba2de0fee8d96273bd10c2465d
und3rdg <und3rdg@gmail.com>
2018-09-18 23:49:57 +0100

    New: demo

---
+++ demo/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>var_dumpPHP demo</title>
+  <script type="module" src="var_dumpPHP.js"></script>
+</head>
+<body>
+<?php
+$testArray = array(
+  key1 => 'test value1',
+  key2 => 'test value2',
+  key3 => 'test value3',
+  key4 => 'test value4',
+);
+?>
+  <h1>var_dumpPHP demo</h1>
+  <p>Ipsum libero suscipit eius cum inventore eius, nulla! Quibusdam expedita iste commodi consequatur adipisci. Saepe laudantium enim vero dolores modi! Nesciunt praesentium facere tempore ullam veniam. Quas quisquam repellat obcaecati delectus dicta Eius non iusto corrupti dolorem perspiciatis Impedit eius dolor dolorum libero sint Fugit rem necessitatibus repellendus eos debitis non minus? Suscipit ab deleniti esse blanditiis quas atque Mollitia sed ex illum exercitationem sint Repellat possimus architecto tempore obcaecati consectetur Voluptatum nam eius error enim quod inventore minima atque. Sequi fugiat culpa laudantium cum ipsam Maiores consequatur ipsa alias assumenda facere! Vero maxime soluta id et suscipit quos?</p>
+  <p>Adipisicing nulla dolores molestiae obcaecati laborum, doloribus exercitationem Minima nihil fugit nisi dolore a Dignissimos est tenetur quos iure quaerat. Ipsa maxime vero ipsa ratione nesciunt blanditiis saepe debitis hico</p>
+  <p>Amet perspiciatis quaerat distinctio quibusdam incidunt Suscipit corporis atque autem nulla maxime magni iste. Quis ipsa impedit nam quidem doloribus!</p>
+  <?php var_dump($testArray); ?>
+  <p>Ipsum libero suscipit eius cum inventore eius, nulla! Quibusdam expedita iste commodi consequatur adipisci. Saepe laudantium enim vero dolores modi! Nesciunt praesentium facere tempore ullam veniam. Quas quisquam repellat obcaecati delectus dicta Eius non iusto corrupti dolorem perspiciatis Impedit eius dolor dolorum libero sint Fugit rem necessitatibus repellendus eos debitis non minus? Suscipit ab deleniti esse blanditiis quas atque Mollitia sed ex illum exercitationem sint Repellat possimus architecto tempore obcaecati consectetur Voluptatum nam eius error enim quod inventore minima atque. Sequi fugiat culpa laudantium cum ipsam Maiores consequatur ipsa alias assumenda facere! Vero maxime soluta id et suscipit quos?</p>
+  <?php var_dump($testArray); ?>
+  <p>Ipsum libero suscipit eius cum inventore eius, nulla! Quibusdam expedita iste commodi consequatur adipisci. Saepe laudantium enim vero dolores modi! Nesciunt praesentium facere tempore ullam veniam. Quas quisquam repellat obcaecati delectus dicta Eius non iusto corrupti dolorem perspiciatis Impedit eius dolor dolorum libero sint Fugit rem necessitatibus repellendus eos debitis non minus? Suscipit ab deleniti esse blanditiis quas atque Mollitia sed ex illum exercitationem sint Repellat possimus architecto tempore obcaecati consectetur Voluptatum nam eius error enim quod inventore minima atque. Sequi fugiat culpa laudantium cum ipsam Maiores consequatur ipsa alias assumenda facere! Vero maxime soluta id et suscipit quos?</p>
+  <?php var_dump($testArray); ?>
+  <p>Ipsum libero suscipit eius cum inventore eius, nulla! Quibusdam expedita iste commodi consequatur adipisci. Saepe laudantium enim vero dolores modi! Nesciunt praesentium facere tempore ullam veniam. Quas quisquam repellat obcaecati delectus dicta Eius non iusto corrupti dolorem perspiciatis Impedit eius dolor dolorum libero sint Fugit rem necessitatibus repellendus eos debitis non minus? Suscipit ab deleniti esse blanditiis quas atque Mollitia sed ex illum exercitationem sint Repellat possimus architecto tempore obcaecati consectetur Voluptatum nam eius error enim quod inventore minima atque. Sequi fugiat culpa laudantium cum ipsam Maiores consequatur ipsa alias assumenda facere! Vero maxime soluta id et suscipit quos?</p>
+</body>
+</html>
