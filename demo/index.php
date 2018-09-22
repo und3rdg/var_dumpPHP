1a29df6d2aa7f3da4c5a99953fe572acc6e88084
und3rdg <und3rdg@gmail.com>
2018-09-22 21:15:27 +0100

    rename again, npm doing problems with desired name

--- demo/index.php
+++ demo/index.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>var_dump_php demo</title>
-  <script type="module" src="var_dump_php.js"></script>
+  <title>var_dumpphp demo</title>
+  <script type="module" src="var_dumpphp.js"></script>
 </head>
 <body>
 <?php
@@ -14,7 +14,7 @@ $testArray = array(
   key4 => 'test value4',
 );
 ?>
-  <h1>var_dump_php demo</h1>
+  <h1>var_dumpphp demo</h1>
   <p>Ipsum libero suscipit eius cum inventore eius, nulla! Quibusdam expedita iste commodi consequatur adipisci. Saepe laudantium enim vero dolores modi! Nesciunt praesentium facere tempore ullam veniam. Quas quisquam repellat obcaecati delectus dicta Eius non iusto corrupti dolorem perspiciatis Impedit eius dolor dolorum libero sint Fugit rem necessitatibus repellendus eos debitis non minus? Suscipit ab deleniti esse blanditiis quas atque Mollitia sed ex illum exercitationem sint Repellat possimus architecto tempore obcaecati consectetur Voluptatum nam eius error enim quod inventore minima atque. Sequi fugiat culpa laudantium cum ipsam Maiores consequatur ipsa alias assumenda facere! Vero maxime soluta id et suscipit quos?</p>
   <p>Adipisicing nulla dolores molestiae obcaecati laborum, doloribus exercitationem Minima nihil fugit nisi dolore a Dignissimos est tenetur quos iure quaerat. Ipsa maxime vero ipsa ratione nesciunt blanditiis saepe debitis hico</p>
   <p>Amet perspiciatis quaerat distinctio quibusdam incidunt Suscipit corporis atque autem nulla maxime magni iste. Quis ipsa impedit nam quidem doloribus!</p>
